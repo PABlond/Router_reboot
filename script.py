ac61b2a44447021939c2c7ce39a90cbd04c0d64e
Pierre-Alexis Blond <34747791+PABlond@users.noreply.github.com>
2019-07-20 17:32:31 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -13,39 +13,43 @@ def setInterval(func,time):
 
 def append_log_file(message): # add a line to your log file 
     with open("internet_failure.log", "a") as logfile:
-        logfile \
-            .write(message)
+        logfile.write(message)
 
 def box_reboot():
     try:
-        password = "<PASSWORD>" # replace with your current password
         binary = FirefoxBinary('/usr/bin/firefox') # replace with your firefox binary filepath
         browser = webdriver.Firefox(firefox_binary=binary)
         browser.get('http://192.168.1.1/reboot')
-        elem_login = browser.find_element_by_id('login')  
-        elem_password = browser.find_element_by_id('password')  
-        elem_login.send_keys('admin')
-        elem_password.send_keys(password)
-        browser.find_element_by_xpath('//button[contains(text(), "Valider")]').click()
-        elem_reboot = browser.find_element_by_xpath('//button[contains(text(), "Redémarrer")]')
-        elem_reboot.click()
-        browser.quit()  
-        append_log_file("reboot at {} \n" \
+        # fulfill login form and access to the reboot page
+        browser \
+            .find_element_by_id('login') \
+            .send_keys('admin') 
+        password = "<PASSWORD>" # replace with your current password
+        browser \
+            .find_element_by_id('password') \
+            .send_keys(password)
+        browser \
+            .find_element_by_xpath('//button[contains(text(), "Valider")]') \
+            .click()
+        # reboot the router and cloe the browser
+        browser \
+            .find_element_by_xpath('//button[contains(text(), "Redémarrer")]') \
+            .click()
+        browser.quit()
+
+        append_log_file("Internet failure at {} \n" \
                     .format(time \
-                        .localtime(time.time())))  
+                        .localtime(time.time()))) 
+        # wait until your router was restarted
         time.sleep(300)
-    except HTTPError as e:
-        print('Your router is not accessible')
-    except URLError as e:
+    except (HTTPError, URLError) as e:
         print('Your router is not accessible')
     
 
 def check_internet():
     try:
         urllib.urlopen('http://www.google.com')
-    except HTTPError as e:
-        box_reboot()
-    except URLError as e:
+    except (HTTPError, URLError) as e:
         box_reboot()
 
 if __name__ == '__main__':
