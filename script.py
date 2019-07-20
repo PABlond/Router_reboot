ce96b1a1baa8bc479a8ff7114ead069c2f4b20ca
Pierre-Alexis Blond <34747791+PABlond@users.noreply.github.com>
2019-07-20 17:24:44 +0200

    add except method in scrapping part

--- script.py
+++ script.py
@@ -11,28 +11,34 @@ def setInterval(func,time):
     while not e.wait(time):
         func()
 
-def append_log_file(): # add a line to your log file 
-    with open("internet_failure.log", "a") as myfile:
-        myfile \
-            .write("reboot at {} \n" \
-                .format(time \
-                    .localtime(time.time())))
+def append_log_file(message): # add a line to your log file 
+    with open("internet_failure.log", "a") as logfile:
+        logfile \
+            .write(message)
 
 def box_reboot():
-    password = <PASSWORD> # replace with your current password
-    binary = FirefoxBinary('/usr/bin/firefox') # replace with your firefox binary filepath
-    browser = webdriver.Firefox(firefox_binary=binary)
-    browser.get('http://192.168.1.1/reboot')
-    elem_login = browser.find_element_by_id('login')  
-    elem_password = browser.find_element_by_id('password')  
-    elem_login.send_keys('admin')
-    elem_password.send_keys(password)
-    browser.find_element_by_xpath('//button[contains(text(), "Valider")]').click()
-    elem_reboot = browser.find_element_by_xpath('//button[contains(text(), "Redémarrer")]')
-    elem_reboot.click()
-    browser.quit()  
-    append_log_file()  
-    time.sleep(300)
+    try:
+        password = "<PASSWORD>" # replace with your current password
+        binary = FirefoxBinary('/usr/bin/firefox') # replace with your firefox binary filepath
+        browser = webdriver.Firefox(firefox_binary=binary)
+        browser.get('http://192.168.1.1/reboot')
+        elem_login = browser.find_element_by_id('login')  
+        elem_password = browser.find_element_by_id('password')  
+        elem_login.send_keys('admin')
+        elem_password.send_keys(password)
+        browser.find_element_by_xpath('//button[contains(text(), "Valider")]').click()
+        elem_reboot = browser.find_element_by_xpath('//button[contains(text(), "Redémarrer")]')
+        elem_reboot.click()
+        browser.quit()  
+        append_log_file("reboot at {} \n" \
+                    .format(time \
+                        .localtime(time.time())))  
+        time.sleep(300)
+    except HTTPError as e:
+        print('Your router is not accessible')
+    except URLError as e:
+        print('Your router is not accessible')
+    
 
 def check_internet():
     try:
@@ -42,4 +48,5 @@ def check_internet():
     except URLError as e:
         box_reboot()
 
-setInterval(check_internet,5)
+if __name__ == '__main__':
+    setInterval(check_internet,5)
